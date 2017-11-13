b13900c634b244fa06c1958d0edbba712e0f7f7d
pacificchillino <pacific.chillino@gmail.com>
2017-11-13 15:03:24 +0800

    20171113c

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Tram Test */
+var HongKongTrams = require("hongkong-trams");
+var trams = HongKongTrams();
+
+var etaeta;
+
+trams.getNextTramETA("92W").then(function(eta){
+  etaeta = eta;
+});
+
+router.get('/trams/eta/:stop', function(req, res) {
+  res.json(etaeta);
+});
+
+//The end
 module.exports = router;
