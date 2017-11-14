7eb9fd6e519a9bf4313c7b512b657e6b6bdef983
pacificchillino <pacific.chillino@gmail.com>
2017-11-14 18:45:18 +0800

    20171114a

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -12,38 +12,88 @@ var HongKongTrams = require("hongkong-trams");
 var trams = new HongKongTrams();
 
 //-- ETA
-router.get('/trams/eta/:stop', function(req, res) {
+function trams_eta_bystop(req, res, isJSON){
   trams.getNextTramETA(req.params.stop).then(function(data) {
-    res.json(data);
-  }).catch(function(err) {
+    if (isJSON){
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data), null, 3);
+    }else{
+      res.render('tram-eta', {title: 'Tram ETA', stop_code: this.stop_code, data: data});
+    }
+  }.bind({stop_code: req.params.stop})).catch(function(err) {
     console.error(err);
   });
+}
+router.get('/api/trams/eta/:stop', function(req, res) {
+  trams_eta_bystop(req, res, true);
+});
+router.get('/test/trams/eta/:stop', function(req, res) {
+  trams_eta_bystop(req, res, false);
+});
+//-- ETA for all stops
+function trams_eta_all(req, res, isJSON){
+	var data = new Object();
+    var promises = new Array();
+    var etaFunction = function(eta){
+      data[this.stopCode].eta = eta;
+    }
+    trams.getTramStops().then(function(stops) {
+      _.each(stops, function(stop) {
+        var stopCode = stop.stop_code;
+        data[stopCode] = stop;
+        promises.push(trams.getNextTramETA(stopCode)
+        .then(etaFunction.bind({stopCode: stopCode}))
+        .catch(function(err) {
+          console.error(err);
+        }));
+      });
+      //Output
+      Promise.all(promises).then(function(){
+        if (isJSON){
+			res.setHeader('Content-Type', 'application/json');
+			res.send(JSON.stringify(data), null, 3);
+		}else{
+			res.render('tram-eta-all', {title: 'Tram ETAs', data: data});
+		}
+      });
+    }).catch(function(err) {
+      console.error(err);
+    });
+}
+router.get('/api/trams/eta', function(req, res) {
+  trams_eta_all(req, res, true);
+});
+router.get('/test/trams/eta', function(req, res) {
+  trams_eta_all(req, res, false);
 });
 //-- Stops
-router.get('/trams/stops', function(req,res){
+router.get('/api/trams/stops', function(req,res){
   trams.getTramStops().then(function(data) {
     for (var i in data){
       data[i] = data[i].stop_code;
     }
     _.uniq(data);
     data.sort();
-    res.json(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data), null, 3);
   }).catch(function(err) {
     console.error(err);
   });
 });
 //-- Service Updates
-router.get('/trams/updates', function(req,res){
+router.get('/api/trams/updates', function(req,res){
   trams.getServiceUpdates().then(function(data) {
-    res.json(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data), null, 3);
   }).catch(function(err) {
     console.error(err);
   });
 });
 //-- Emergency Message (EM)
-router.get('/trams/em/:stop', function(req,res){
+router.get('/api/trams/em/:stop', function(req,res){
   trams.getEmergencyMessageForTramStop(req.params.stop).then(function(data) {
-    res.json(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data), null, 3);
   }).catch(function(err) {
     console.error(err);
   });
@@ -52,9 +102,10 @@ router.get('/trams/em/:stop', function(req,res){
 //Weather Test
 var HongKongWeather = require('hongkong-weather');
 var weather = new HongKongWeather();
-router.get('/weather', function(req,res){
+router.get('/api/weather', function(req,res){
   weather.getCurrent().then(function(data){
-    res.json(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data), null, 3);
   }).catch(console.error);
 });
 
