fe2fc7141c6b26bee3fcc2707fd83b546832b377
pacificchillino <pacific.chillino@gmail.com>
2017-11-28 11:59:27 +0800

    20171128 1159 minor fix

--- myapp/include/fixtime-20171128.js
+++ myapp/include/fixtime-20171128.js
@@ -3,11 +3,10 @@ exports.fixTime = function(from, to){
 		if (err) throw err;
 		for (var i in result){
 			var filter = {_id: result[i]._id};
-			var newVal = {
-				hours: result[i].hours - 24,
-				hours0: result[i].hours0 - 24,
-				hours1: result[i].hours1 - 24,
-			};
+			var newVal = result[i];
+			newVal.hours = result[i].hours - 24;
+			newVal.hours0 = result[i].hours0 - 24;
+			newVal.hours1 = result[i].hours1 - 24;
 			global.db.collection("data_tram_" + from + "_" + to).updateOne(filter, newVal, function(err, res) {
 				if (err) throw err;
 			});
